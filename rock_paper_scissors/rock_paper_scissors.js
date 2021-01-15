2aee66d1dccfbb6b9e04f59098c3caf3d532c9fc
daniel2234 <kwiatkowski.daniel@gmail.com>
2021-01-14 23:01:31 -0500

    started a command line rock paper scissors game

--- rock_paper_scissors/rock_paper_scissors.js
+++ rock_paper_scissors/rock_paper_scissors.js
@@ -1,58 +1,15 @@
+//Rock Paper Scissors
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const VALID_CHOICES = ['rock', 'paper', 'scissors'];
 
 function prompt(message) {
-  console.log(`=> ${message}`)
+  console.log(`=> ${message}`);
 }
 
-function displayWinner() {
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+let choice = readline.question();
 
-  if((choice === 'rock' && computerChoice === 'scissors') ||
-     (choice === 'paper' && computerChoice === 'rock') ||
-     (choice === 'scissors' && computerChoice === 'paper')) {
-      prompt('You win!');
-  } else if ((choice === 'rock' && computerChoice === 'paper') ||
-              (choice === 'paper' && computerChoice === 'scissors') ||
-              (choice === 'scissors' && computerChoice === 'rock')) {
-      prompt('Computer wins!');
-  } else {
-    prompt("It's a tie");
-  }
+while (!VALID_CHOICES.includes(choice)) {
+  prompt("Thats not a valid choice");
+  choice = readline.question();
 }
-
-while(true) {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
-
-  while(!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
-    choice = readline.question();
-  }
-
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
-
-displayWinner(choice, computerChoice);
-
-  prompt('Do you want to play again (y/n)?');
-  let answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt('Please enter "y" or "n".');
-    answer = readline.question().toLowerCase();
-  }
-
-  if(answer[0] !== 'y') break;
-}
-
-
-// Here are a couple of bonus features for the Rock Paper Scissors program.
-
-
-// Shortened Input Typing the full word "rock" or "lizard" is tiring. Update the program so the user can type "r" for "rock," "p" for "paper," and so on. Note that if you do bonus #1, you'll have two words that start with "s." How do you resolve that?
-
-// Best of 5 Keep score of the player's and computer's wins. When either the player or computer reaches five wins, the match is over, and the winning player becomes the grand winner. Don't add your incrementing logic to displayResults. Keep your functions simple; they should perform one logical task — no more, no less.
-
-// ESLint complaints Run your program through ESLint to see what problems it identifies, and try to fix them.
-
-//Consider asking for a code review Be sure to ask questions about any part of your code that you're uncertain about.
